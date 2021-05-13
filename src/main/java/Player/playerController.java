925142339281fb7f7aa8dc541d84cda99af531c1
Midnightknight <44758301+Midnightknight@users.noreply.github.com>
2021-05-13 15:34:33 -0400

    I added text fields next to the sliders. The values of the slider will now show in the text fields. Also, I fix the error on the media player code, but the video still won't display.

--- src/main/java/Player/playerController.java
+++ src/main/java/Player/playerController.java
@@ -3,13 +3,16 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaView;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.web.HTMLEditor;
+
+
 import java.io.File;
 
-public class playerController<mediaPlayer> {
+public class playerController{
 
     @FXML
     private Button SubmitButton;
@@ -42,6 +45,32 @@ public class playerController<mediaPlayer> {
     private Slider Slider4;
 
     @FXML
+    private TextField ScoreField_1;
+
+    @FXML
+    private TextField ScoreField_2;
+
+    @FXML
+    private TextField ScoreField_3;
+
+    @FXML
+    private TextField ScoreField_4;
+
+    @FXML
+    void onSliderChanged(javafx.scene.input.MouseEvent event) {
+        /*int sliderValue_1 = (int) Slider1.getValue();
+        int sliderValue_2= (int) Slider1.getValue();
+        int sliderValue_3= (int) Slider1.getValue();
+        int sliderValue_4= (int) Slider1.getValue();*/
+        //System.out.println(sliderValue_1 + " ");
+        ScoreField_1.setText(Slider1.getValue() + " ");
+        ScoreField_2.setText(Slider2.getValue() + " ");
+        ScoreField_3.setText(Slider3.getValue() + " ");
+        ScoreField_4.setText(Slider4.getValue() + " ");
+
+    }
+
+    @FXML
     private Label Ename;
 
     @FXML
@@ -53,8 +82,17 @@ public class playerController<mediaPlayer> {
     @FXML
     private Button BackButton;
 
+    //Instantiating the media field.
+    String Spath = "C:/Users/Kenneth/Videos/Twitch/Test.mp4";
+
+    //Instantiating Media Class
+    Media media = new Media(new File(Spath).toURI().toString());
+
+    //Instantiating media player
+    MediaPlayer mediaPlayer = new MediaPlayer(media);
+
     @FXML
-    public MediaView videoPlayer;
+    public MediaView videoPlayer = new MediaView(mediaPlayer);
 
     @FXML
     private Slider ProgressBar;
@@ -65,17 +103,8 @@ public class playerController<mediaPlayer> {
     @FXML
     private Button PauseButton;
 
-    //Instantiating the media field.
-    String Spath = "C:/Users/Kenneth/Videos/Twitch/Test.mp4";
-
-    //Instantiating Media Class
-    Media media = new Media(new File(Spath).toURI().toString());
-
-    //Instantiating media player
-    MediaPlayer mediaPlayer = new MediaPlayer(media);
-
     //Instantiating mediaView
-     videoPlayer.setMediaPlayer(mediaPlayer);
+
 
 
 }
